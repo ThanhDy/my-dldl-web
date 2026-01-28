028eb9c02026b7241ed62b9b1e5b3d27360b590d
ThanhDy <thanhduyct03@gmail.com>
2026-01-28 16:00:00 +0700

    install mongodb

---
+++ lib/mongodb.ts
@@ -0,0 +1,36 @@
+// lib/db.ts
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("Vui lòng khai báo MONGODB_URI trong file .env.local");
+}
+
+// Dùng kỹ thuật caching connection để tránh spam database khi reload
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI!).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
+
+export default dbConnect;
